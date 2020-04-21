3b70fa0b30bd878d47ef8e7e1347eb4c3821d6a3
TurtleTheSeaHobo <james@enfuego.us>
2020-04-21 20:26:01 +0100

    Updated IMU driver module

--- lib/bluemage/imu-driver.ex
+++ lib/bluemage/imu-driver.ex
@@ -54,5 +54,54 @@ defmodule Bluemage.IMU do
 	end
 
 	#Start the FXAS setup ritual with the given angular velocity range (+/- DPS) and frequency (Hz)
-	#def start_FXAS(ref, dev, range, frequency) do
+	def start_FXAS(ref, dev, range, frequency) do
+		#Set CTRL_REG1 to 0000 0000 to enter standby mode
+		I2C.write(ref, dev, <<0x13, 0x00>>)
+		#Set CTRL_REG1 to 0100 0000 to perform full reset
+		I2C.write(ref, dev, <<0x13, 0x40>>)
+		#Set CTRL_REG0 to 0000 00xx to select angular velocity range
+		case range do
+			250	-> I2C.write(ref, dev, <<0x0D, 0x03>>)
+			500	-> I2C.write(ref, dev, <<0x0D, 0x02>>)
+			1000	-> I2C.write(ref, dev, <<0x0D, 0x01>>)
+			2000	-> I2C.write(ref, dev, <<0x0D, 0x00>>)
+		end
+		#Set CTRL_REG1 to 000x xx10 to select frequency and enter active mode
+		case frequency do
+			12.5	-> I2C.write(ref, dev, <<0x13, 0x1A>>)
+			25.0	-> I2C.write(ref, dev, <<0x13, 0x16>>)
+			50.0	-> I2C.write(ref, dev, <<0x13, 0x12>>)
+			100.0	-> I2C.write(ref, dev, <<0x13, 0x0E>>)
+			200.0	-> I2C.write(ref, dev, <<0x13, 0x0A>>)
+			400.0	-> I2C.write(ref, dev, <<0x13, 0x06>>)
+			800.0	-> I2C.write(ref, dev, <<0x13, 0x02>>)
+		end
+		:ok
+	end
+
+	#Get the current FXAS gyroscope data
+	def get_FXAS_data(ref, dev, range) do
+		#Set STATUS to 1000 0000 for... some reason (idk, Adafruit does it)
+		I2C.write(ref, dev, <<0x00, 0x80>>)
+		#Read 7 bytes from starting register 0x00
+		<<_status, xhi, xlo, yhi, ylo, zhi, zlo>> = I2C.write_read!(ref, dev, <<0x00>>, 7)
+		#Recombobulate, convert, and return those numbers (rad/s units)
+		[
+		gx: to_signed((xhi <<< 8) ||| xlo, 16) * div(range, 250) * 0.0001363538515625,
+		gy: to_signed((yhi <<< 8) ||| ylo, 16) * div(range, 250) * 0.0001363538515625,
+		gz: to_signed((zhi <<< 8) ||| zlo, 16) * div(range, 250) * 0.0001363538515625
+		]
+	end
+
+	#Start both sensor setup rituals with given ranges on a single frequency
+	def start_IMU(ref, g_dev, a_dev, g_range, a_range, frequency) do
+		start_FXAS(ref, g_dev, g_range, frequency)
+		start_FXOS(ref, a_dev, a_range, frequency)
+		:ok
+	end
+
+	#Get the current IMU data in a quick-succession read
+	def get_IMU_data(ref, g_dev, a_dev, g_range, a_range) do
+		get_FXAS_data(ref, g_dev, g_range) ++ get_FXOS_data(ref, a_dev, a_range)
+	end
 end
